f126ca73426125c0bf21d5b4900194d64ea98d56
Vikkelas <bass89@mail.ru>
2022-02-09 12:47:06 +0300

    copy notbook

--- src/js/Cards.js
+++ src/js/Cards.js
@@ -1,16 +1,15 @@
 /* eslint-disable linebreak-style */
-import addCard from './utilits';
-
 export default class Cards {
   constructor() {
-    this.boxsCards = document.querySelectorAll('.task-box');
     this.btnsCreatCard = document.querySelectorAll('.task-box__btn');
-    this.addCard = addCard;
     this.boxNewCard = null;
+    this.cards = [...document.querySelectorAll('.task-box__card')];
   }
 
   init() {
     this.btnsCreatCard.forEach((item) => item.addEventListener('click', this.createCard.bind(this)));
+    this.deletCard();
+    this.dragAndDrops();
   }
 
   createCard(e) {
@@ -22,22 +21,44 @@ export default class Cards {
        <button class="new-card__btn-delet">&#10006</button>
     </div>
     </div>`;
+    this.showBtnAdd();
+    if (this.boxNewCard === null) {
+      e.target.classList.add('task-box__btn--none');
+      this.showTextarea(e);
+      this.boxNewCard = '.new-card';
+      this.onClickBtn();
+    } else if (this.boxNewCard !== null) {
+      e.target.classList.add('task-box__btn--none');
+      this.hidingTextarea();
+      this.showTextarea(e);
+      this.onClickBtn();
+    }
+  }
+
+  deletCard() {
+    this.cards.forEach((item) => {
+      item.addEventListener('mouseenter', (e) => {
+        const btnClose = e.currentTarget.querySelector('.task-box__card-btn');
+        btnClose.classList.add('task-box__card-btn--active');
+        btnClose.addEventListener('click', (e) => {
+          const cardDelet = e.target.closest('div.task-box__card');
+          const boxItems = e.target.closest('.task-box__content');
+          boxItems.removeChild(cardDelet);
+        });
+      });
+      item.addEventListener('mouseleave', (e) => {
+        const btnClose = e.target.querySelector('.task-box__card-btn');
+        btnClose.classList.remove('task-box__card-btn--active');
+      });
+    });
+  }
+
+  showBtnAdd() {
     this.btnsCreatCard.forEach((item) => {
       if (item.classList.contains('task-box__btn--none')) {
         item.classList.remove('task-box__btn--none');
       }
     });
-    if (this.boxNewCard === null) {
-      e.target.classList.add('task-box__btn--none');
-      this.showTextarea(e);
-      this.addCard();
-      this.boxNewCard = '.new-card';
-    } else if (this.boxNewCard !== null) {
-      e.target.classList.add('task-box__btn--none');
-      this.hidingTextarea();
-      this.showTextarea(e);
-      this.addCard();
-    }
   }
 
   showTextarea(e) {
@@ -49,5 +70,60 @@ export default class Cards {
     const deletEl = document.querySelector(this.boxNewCard);
     const parentDelEl = deletEl.closest('.task-box');
     parentDelEl.removeChild(deletEl);
+  }
+
+  onClickBtn() {
+    const newCard = document.querySelector(this.boxNewCard);
+    newCard.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (e.target.classList.contains('new-card__btn-delet')) {
+        this.hidingTextarea();
+        this.showBtnAdd();
+        this.boxNewCard = null;
+      }
+      if (e.target.classList.contains('new-card__btn-add')) {
+        const parent = e.currentTarget.closest(this.boxNewCard);
+        const boxContent = e.currentTarget.closest('.task-box').querySelector('.task-box__content');
+        const text = parent.querySelector('.new-card__text');
+        const element = `
+        <div class="task-box__card">
+          <div class="task-box__card-text"><span class="task-box__card-btn">close</span>
+          ${text.value}
+          </div>
+        </div> `;
+        boxContent.insertAdjacentHTML('beforeend', element);
+        this.hidingTextarea();
+        this.showBtnAdd();
+        this.boxNewCard = null;
+        this.cards = [...document.querySelectorAll('.task-box__card')];
+        this.deletCard();
+        this.dragAndDrops();
+      }
+    });
+  }
+
+  dragAndDrops() {
+    let draggedEl = null;
+    let ghostEl = null;
+    const itemsEl = document.querySelector('.body__container');
+    itemsEl.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+      if (!e.target.classList.contains('task-box__card')) {
+        return;
+      }
+      draggedEl = e.target;
+      ghostEl = e.target.cloneNode(true);
+      ghostEl.classList.add('task-box__card--dragged');
+      document.body.appendChild(ghostEl);
+      ghostEl.style.left = `${e.pageX - ghostEl.offsetWidth / 2}px`;
+      ghostEl.style.top = `${e.pageX - ghostEl.offsetHeight / 2}px`;
+    });
+    itemsEl.addEventListener('mousemove', (e) => {
+      if (!draggedEl) {
+        return;
+      }
+      ghostEl.style.left = `${e.pageX - ghostEl.offsetWidth / 2}px`;
+      ghostEl.style.top = `${e.pageX - ghostEl.offsetHeight / 2}px`;
+    });
   }
 }
